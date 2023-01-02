55acfacc7389a345d6e0cf71571ec928d572de92
Agathius <p.akos1148@gmail.com>
2023-01-02 14:57:35 +0100

    added cors

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const connectDB = require('./db/connect');
 require('dotenv').config(); // access .env contents
 const notFound = require('./middleware/not-found.js'); 
 const errorHandlerMiddleware = require('./middleware/error-handler.js'); 
-
+const cors = require('cors');
 const port = process.env.PORT || 3000;
+
 //middleware
 app.use(express.json());  //built in middleware function in Express . It parses incoming JSON requests and puts the parsed data in req.body.
 app.use(errorHandlerMiddleware);
+app.use(cors({ // allow cors for a specific origin 
+  origin: 'https://task-manager-client.onrender.com'
+}))
 //routes
 app.use('/api/v1/tasks', tasks);
 app.use('/api/v1/schema', schema);
 app.use(notFound); // if route is not found
+
 
 const startServer = async () => {
   try {
